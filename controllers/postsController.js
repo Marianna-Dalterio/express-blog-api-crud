2f47c5315d0127ada70e68ec08ef309bb32555f8
Marianna-Dalterio <mariannadalterio00@gmail.com>
2025-09-12 22:23:49 +0200

    destroy route implementation

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -7,7 +7,15 @@ const create = (req, res) => {
     res.send("Creazione nuovo post")
 };
 const show = (req, res) => {
-    res.send("Dettagli della pizza" + req.params.id)
+    // res.send("Dettagli della pizza" + req.params.id)
+    const id = req.params.id;
+    const post = posts.find(item => item.id === parseInt(id));
+    if (!post) {
+        return res.status(404).json({
+            error: "Not found"
+        })
+    }
+    res.send(post);
 };
 const update = (req, res) => {
     res.send("Update a single post with ID:" + req.params.id)
@@ -16,7 +24,19 @@ const modify = (req, res) => {
     res.send("Partial update of a single post with ID:" + req.params.id)
 };
 const destroy = (req, res) => {
-    res.send("Delete the single post with ID:" + req.params.id)
+    // res.send("Delete the single post with ID:" + req.params.id)
+    const id = parseInt(req.params.id);
+    const post = posts.find(item => item.id === id);
+    if (!post) {
+        res.status(404);
+        return res.json({
+            status: "404",
+            error: "Not found",
+            message: "Pizza non trovata"
+        })
+    }
+    posts.splice(posts.indexOf(post), 1);
+    res.sendStatus(204);
 };
 
 
